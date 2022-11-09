fe0c27860e64399478f6912004264b3a1c3c56c0
Aleksander Krauze <alkoszczak@gmail.com>
2022-11-10 00:44:34 +0100

    Minimal executable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn handler(_: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
