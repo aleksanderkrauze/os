108401024cb51d0cb258b242670d1bb971e84101
Aleksander Krauze <alkoszczak@gmail.com>
2022-11-10 12:05:10 +0100

    Print test status and results to Serial Port

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use core::panic::PanicInfo;
 
 use x86_64::instructions::port::Port;
 
+mod serial;
 mod vga_buffer;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -26,9 +27,20 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn handler(info: &PanicInfo) -> ! {
     println!("{}", info);
+
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}", info);
+    exit_qemu(QemuExitCode::Failed);
 
     loop {}
 }
@@ -61,11 +73,26 @@ pub extern "C" fn _start() -> ! {
 mod tests {
     use super::*;
 
-    pub fn test_runner(tests: &[&dyn Fn()]) {
-        println!("Running {} tests", tests.len());
+    pub trait Testable {
+        fn run(&self);
+    }
+
+    impl<T> Testable for T
+    where
+        T: Fn(),
+    {
+        fn run(&self) {
+            serial_print!("{}...\t", core::any::type_name::<T>());
+            self();
+            serial_println!("[ok]");
+        }
+    }
+
+    pub fn test_runner(tests: &[&dyn Testable]) {
+        serial_println!("Running {} tests", tests.len());
 
         for test in tests {
-            test();
+            test.run();
         }
 
         exit_qemu(QemuExitCode::Success);
@@ -73,8 +100,6 @@ mod tests {
 
     #[test_case]
     fn trivial_assertion() {
-        print!("trivial assertion... ");
         assert_eq!(1, 1);
-        println!("[ok]");
     }
 }
