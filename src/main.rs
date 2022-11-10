5ebcc4d9766dc868fc98fac2c221737a642dee89
Aleksander Krauze <alkoszczak@gmail.com>
2022-11-10 03:28:51 +0100

    Use VGAWriter::set_color

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,12 @@ fn handler(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello {}!", "World");
     println!();
+    vga_buffer::WRITER
+        .lock()
+        .set_color(vga_buffer::ColorCode::new(
+            vga_buffer::Color::LightRed,
+            vga_buffer::Color::Black,
+        ));
     println!("Here is some data: {}, {}", 42, 2.0 / 3.0);
 
     loop {}
