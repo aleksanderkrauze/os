a36e194742fe95fea1802aa11848fcd1425c98a0
Aleksander Krauze <alkoszczak@gmail.com>
2022-11-10 03:43:01 +0100

    Create simple test runner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::tests::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -12,6 +15,15 @@ fn handler(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello {}!", "World");
@@ -25,4 +37,23 @@ pub extern "C" fn _start() -> ! {
     println!("Here is some data: {}, {}", 42, 2.0 / 3.0);
 
     loop {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    pub fn test_runner(tests: &[&dyn Fn()]) {
+        println!("Running {} tests", tests.len());
+        for test in tests {
+            test();
+        }
+    }
+
+    #[test_case]
+    fn trivial_assertion() {
+        print!("trivial assertion... ");
+        assert_eq!(1, 1);
+        println!("[ok]");
+    }
 }
